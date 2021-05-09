a2d35b293b5d075bf8f1f9a77560b3eb26f6453a
MrXogos <49158267+Xogy@users.noreply.github.com>
2021-05-09 22:30:29 +0200

    Create fr.lua

---
+++ locales/fr.lua
@@ -0,0 +1,25 @@
+Locales['fr'] = {
+	["ticket_expired"]  = "Votre ticket à expiré",
+	["time_left_count"] = "Temps restant %.2d:%.2d",
+
+	["player_left_arcade"] = "Vous avez quité la salle de jeu, votre ticket est devenu invalide.",
+	["need_to_buy_ticket"] = "Je devrait d'abord acheter un ticket !",
+	["open_ticket_menu"]   = "Appuyez sur ~INPUT_CONTEXT~ pour discuter avec le mec cool.",
+	["open_computer"]      = "Appuyez sur ~INPUT_CONTEXT~ pour démarer l'ordinateur",
+	["not_enough_money"]   = "Malheureusement, vous n'avez pas assez d'argent.",
+	["bought_ticket"]      = "Vous avez acheté un ticket %s, passez un bon moment dans la salle d'arcade. Vous avez: %s minutes",
+
+	["bronz"]  = "bronze",
+	["silver"] = "argent",
+	["gold"]   = "or",
+
+	["ticket_label"] = "ticket %s",
+
+	["ticket_menu"]     = "Acheter un ticket",
+	["computer_menu"]   = "Menu de l'ordinateur",
+
+	["give_back_ticket"]   = "Voulez-vous rendre votre ticket ?",
+
+	["yes"] = "oui",
+	["no"] = "non",
+}
